9daeba12c261251dd91c995ff9557d03a7d53217
ThivyashreeNS <nstshree2000@gmail.com>
2024-07-26 21:06:44 +0530

    Add files via upload

---
+++ Problem 2.py
@@ -0,0 +1,17 @@
+""" Problem 1: Write a python function to read from a text file.
+               The function will take the name of the text file and display the content of the file."""
+ 
+# Defined a function with file_name as parameter
+def read_file(file_name):
+    
+    # Opens the file in read mode
+    file=open(file_name,'r')
+    
+    # Reads the content of the file and saves it in display
+    display=file.read()
+    
+    # Prints the content
+    print(display)
+
+# Calling the function with file name as parameter
+read_file("file_1.txt")
